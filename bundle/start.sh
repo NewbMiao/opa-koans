579899e5fe45bfb3820c06cd8a38eb59ada5e50c
newbmiao <newbvirgil@gmail.com>
2020-04-10 15:55:24 +0800

    update monitor start.sh and format yaml

--- bundle/start.sh
+++ bundle/start.sh
@@ -2,19 +2,41 @@
 
 workspace=$(cd $(dirname $0) && pwd -P)
 
-{
-    # bundle
-    cd $workspace/example
-    find . -type f ! -name "*.tar.gz" | xargs tar -czf rbac.tar.gz
-    mv rbac.tar.gz ../docker-compose/demo-server
-    echo "rbac files bundled!"
-    
-    # discovery
-    cd $workspace/discovery
-    find . -type f ! -name "*.tar.gz" | xargs tar -czf discovery.tar.gz
-    mv discovery.tar.gz ../docker-compose/demo-server
-    echo "discovery files bundled!"
+start() {
+  # bundle
+  cd $workspace/example
+  find . -type f ! -name "*.tar.gz" | xargs tar -czf rbac.tar.gz
+  mv rbac.tar.gz ../docker-compose/demo-server
+  echo "rbac files bundled!"
 
+  # discovery
+  cd $workspace/discovery
+  find . -type f ! -name "*.tar.gz" | xargs tar -czf discovery.tar.gz
+  mv discovery.tar.gz ../docker-compose/demo-server
+  echo "discovery files bundled!"
+
+  cd $workspace/docker-compose
+  exec docker-compose -f docker-compose-monitor.yaml up -d
+}
+
+action="$1"
+{
+
+  case $action in
+  "start")
+    start
+    ;;
+  "stop")
     cd $workspace/docker-compose
-    exec docker-compose -f docker-compose-monitor.yaml up
+    exec docker-compose -f docker-compose-monitor.yaml stop
+    ;;
+  "logs")
+    [ "$2" == "" ] && echo "Please specify one below container name to see log:\n $(docker ps -a -f network=docker-compose_monitor)" && exit 1
+    cId=$(docker ps -a | grep $2 | cut -d ' ' -f1)
+    [ "$cId" != "" ] && docker logs -ft $cId
+    ;;
+  *)
+    echo "Usage:\n\tsupport cmd is: start, stop, logs <container name>"
+    ;;
+  esac
 }
