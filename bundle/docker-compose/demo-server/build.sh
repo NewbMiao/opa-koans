971c6c95dbb70df788bcb30baf54a1c6118487cc
newbmiao <newbvirgil@gmail.com>
2020-05-22 08:02:07 +0800

    update bundle build with opa v0.20

--- bundle/docker-compose/demo-server/build.sh
+++ bundle/docker-compose/demo-server/build.sh
@@ -7,46 +7,43 @@ ENV_OS="${1:-$CUR_OS}"
 GO_CACHE_DIR="$workspace"/.go-cache
 bundleDir="$workspace"/../..
 
-opa_revision() {
+opa_bundle() {
     REVISION_BUNDLE=$(git log --abbrev-commit --pretty=format:"%h" "$bundleDir"/example | head -n1)
     REVISION_DISCOVERY=$(git log --abbrev-commit --pretty=format:"%h" "$bundleDir"/discovery | head -n1)
 
-    # update .manifest revision
-    echo "Checking revision of rbac's .manifest..."
-    jq --arg rid "$REVISION_BUNDLE" '.revision = $rid' "$bundleDir"/example/.manifest | tee tmp_revison
-    mv tmp_revison "$bundleDir"/example/.manifest
-    echo "Updated revision of rbac's .manifest"
+    # # update .manifest revision
+    # echo "Checking revision of rbac's .manifest..."
+    # jq --arg rid "$REVISION_BUNDLE" '.revision = $rid' "$bundleDir"/example/.manifest | tee tmp_revison
+    # mv tmp_revison "$bundleDir"/example/.manifest
+    # echo "Updated revision of rbac's .manifest"
 
     cat >"$workspace"/revision.env <<EOF
 # demo-server env_file: revision of each bundle, used for etag
 REVISION_BUNDLE=$REVISION_BUNDLE
 REVISION_DISCOVERY=$REVISION_DISCOVERY
 EOF
-}
-opa_bundle() {
     dockerComposeDir="$workspace"/..
     # bundle
-    cd "$bundleDir"/example || exit
-    find . -type f ! -name "*.tar.gz" -print0 | tar -cvzf rbac.tar.gz --null -T -
-    mv rbac.tar.gz "$dockerComposeDir/demo-server"
+    cd "$bundleDir" || exit
+    # enable partical evaluation optimize for bundle build
+    opa build -b example -o "$dockerComposeDir/demo-server"/rbac.tar.gz -t rego -O 1 -e rbac/allow -r "$REVISION_BUNDLE"
     echo "RBAC files bundled!"
+    # find . -type f ! -name "*.tar.gz" -print0 | tar -cvzf rbac.tar.gz --null -T -
+    # mv rbac.tar.gz "$dockerComposeDir/demo-server"
 
     # discovery
-    cd "$bundleDir"/discovery || exit
-    find . -type f ! -name "*.tar.gz" -print0 | tar -cvzf discovery.tar.gz --null -T -
-    mv discovery.tar.gz "$dockerComposeDir/demo-server"
+    opa build -b discovery -o "$dockerComposeDir/demo-server"/discovery.tar.gz -t rego
     echo "Discovery files bundled!"
 }
 
 {
-    opa_revision
     echo "start build opa bundle ..."
     opa_bundle
     echo "start build demo-app ..."
     cd "$workspace" || exit
 
     export GOPROXY=https://goproxy.cn,direct
-    GOPATH="$GO_CACHE_DIR" CGO_ENABLED=0 GOOS="$ENV_OS" GOARCH=amd64 go build -o demo-app main.go opa.go
+    GOPATH="$GO_CACHE_DIR" CGO_ENABLED=0 GOOS="$ENV_OS" GOARCH=amd64 go build -ldflags="-s -w" -o demo-app main.go opa.go
     echo "go mod cache in .go-cache:"
     ls "$GO_CACHE_DIR"/pkg/mod
     echo "demo-app built!"
