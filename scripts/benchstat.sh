9b047a8c13cbff98a3ca09e48868c626bb0ac334
newbmiao <newbvirgil@gmail.com>
2020-05-28 21:50:14 +0800

    update benchstat

--- scripts/benchstat.sh
+++ scripts/benchstat.sh
@@ -9,14 +9,13 @@ workspace=$(cd "$(dirname "$0")" && pwd -P)
     docker run --rm -v "$(pwd)":/code -w /code openpolicyagent/opa:latest build -b example -o rbac-optmized.tar.gz -O 1 -e rbac/allow -t rego
     echo "rbac files bundled with optmization!"
 
-    cnt=1
+    cnt=3
     cp "$workspace/../quick-start/input.json" rbac-input.json
-    echo "benchmark orignal rbac bundle"
+    echo "benchmark orignal rbac bundle..."
     docker run --rm -v "$(pwd)":/code -w /code openpolicyagent/opa:latest bench -f gobench -b rbac-raw.tar.gz --count "$cnt" -i rbac-input.json --benchmem 'data.rbac.allow' >rbac-raw.txt
-    echo "benchmark optmized rbac bundle"
+    echo "benchmark optmized rbac bundle..."
     docker run --rm -v "$(pwd)":/code -w /code openpolicyagent/opa:latest bench -f gobench -b rbac-optmized.tar.gz --count "$cnt" -i rbac-input.json --benchmem 'data.rbac.allow' >rbac-optmized.txt
     echo "use benchstat to compare them:"
-    go get -u golang.org/x/perf/cmd/benchstat
-    benchstat rbac-raw.txt rbac-optmized.txt | tee bench.txt
+    docker run --rm -v "$(pwd)":/code -w /code newbmiao/benchstat:v1 rbac-raw.txt rbac-optmized.txt | tee bench.txt
     rm rbac-*.txt rbac-*.json rbac-*.tar.gz
 }
